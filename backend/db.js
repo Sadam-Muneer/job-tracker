adc9ee7f1e35dd346d9c11d36aefd24ea7895dee
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-07-29 10:38:04 +0500

    updated

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
                     try {
-                                        mongoose.connect("mongodb://localhost:27017/jobApp", {
+                                        mongoose.connect("mongodb://sadammuneer:sadam%40123456@ac-fmojhku-shard-00-00.07bagy7.mongodb.net:27017,ac-fmojhku-shard-00-01.07bagy7.mongodb.net:27017,ac-fmojhku-shard-00-02.07bagy7.mongodb.net:27017/jobApp?ssl=true&replicaSet=atlas-dha073-shard-0&authSource=admin&retryWrites=true&w=majority", {
                                         });
 
                                         console.log("MongoDB connected.");
